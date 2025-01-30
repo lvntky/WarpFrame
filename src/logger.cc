e2a172c78115a899e885393f7187ba7c105777f8
Levent Kaya <42411502+lvntky@users.noreply.github.com>
2025-01-30 16:26:38 +0300

    [fix] logger level issue fix

--- src/logger.cc
+++ src/logger.cc
@@ -3,10 +3,13 @@
 std::shared_ptr<spdlog::logger> logger;
 
 void Logger::init() {
-  logger = spdlog::basic_logger_mt("WarpFrameLogger", "warpframe.log", true);
-  spdlog::set_pattern("[%d-%m-%Y %H:%M:%S] [%^%1%$] %v");
-  logger->set_level(spdlog::level::info);
-  spdlog::info("Logger initialized.");
+    logger = spdlog::basic_logger_mt("WarpFrameLogger", "warpframe.log", true);
+    spdlog::set_pattern("[%d-%m-%Y %H:%M:%S] [%^%L%$] %v");
+    logger->set_level(spdlog::level::info);
+
+    spdlog::set_default_logger(logger);
+
+    spdlog::info("Logger initialized.");
 }
 
 std::shared_ptr<spdlog::logger> Logger::getLogger() { return logger; }
