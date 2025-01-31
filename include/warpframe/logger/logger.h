044335dbd2466e36977b1e5a200cf6998284bf93
Levent Kaya <levent@dev>
2025-01-31 10:58:04 +0300

    [fix] logger issue fixed

--- include/warpframe/logger/logger.h
+++ include/warpframe/logger/logger.h
@@ -3,12 +3,16 @@
 
 #include <memory>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 class Logger {
 public:
   static void init();
   static std::shared_ptr<spdlog::logger> getLogger();
+
+private:
+  static std::shared_ptr<spdlog::logger> logger;
 };
 
 #endif // WARPFRAME_LOGGER_H_
--- src/logger.cc
+++ src/logger.cc
@@ -1,15 +1,31 @@
+#include <iostream>
+#include <vector>
 #include <warpframe/logger/logger.h>
 
-std::shared_ptr<spdlog::logger> logger;
+std::shared_ptr<spdlog::logger> Logger::logger;
 
 void Logger::init() {
-    logger = spdlog::basic_logger_mt("WarpFrameLogger", "warpframe.log", true);
-    spdlog::set_pattern("[%d-%m-%Y %H:%M:%S] [%^%L%$] %v");
+  try {
+    // Create vector of sinks
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+        "warpframe.log", true);
+
+    std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
+
+    logger = std::make_shared<spdlog::logger>("WarpFrameLogger", sinks.begin(),
+                                              sinks.end());
+
+    logger->set_pattern("[%d-%m-%Y %H:%M:%S] [%^%l%$] %v");
+
     logger->set_level(spdlog::level::info);
 
     spdlog::set_default_logger(logger);
 
-    spdlog::info("Logger initialized.");
+    logger->info("Logger initialized with console and file output.");
+  } catch (const spdlog::spdlog_ex &ex) {
+    std::cerr << "Logger initialization failed: " << ex.what() << std::endl;
+  }
 }
 
 std::shared_ptr<spdlog::logger> Logger::getLogger() { return logger; }
